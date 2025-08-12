c8b554f4c0dbb94f18c5e38bd5b39bfe0e98bd52
EJ676 <esipovich@gmail.com>
2025-08-12 22:58:01 +0300

    урок 12-15

---
+++ snapshots/refs/ref_city_region.sql
@@ -0,0 +1,20 @@
+{% snapshot ref_city_region %}
+
+{{
+   config(
+       target_schema='snapshot',
+       unique_key='city',
+
+       strategy='timestamp',
+       updated_at='updated_at',
+   )
+}}
+
+SELECT
+    city,
+    region,
+    updated_at
+FROM
+    {{ref("city_region")}}
+
+{% endsnapshot %}
